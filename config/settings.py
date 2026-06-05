14fdd19d223b9b00f2eb19381319b2381256cd41
Eshukurullohdev <shukurullohergashev2@gmail.com>
2026-06-05 08:54:20 +0500

    user test

--- config/settings.py
+++ config/settings.py
@@ -78,12 +78,17 @@ WSGI_APPLICATION = 'config.wsgi.application'
 import os
 import dj_database_url
 
-DATABASES = {
-    'default': dj_database_url.parse(
-        os.environ.get("DATABASE_URL")
-        or os.environ.get("DATABASE_PUBLIC_URL")
-    )
-}
+if os.environ.get("DATABASE_URL"):
+    DATABASES = {
+        "default": dj_database_url.parse(os.environ.get("DATABASE_URL"))
+    }
+else:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+        }
+    }
 
 
 # Password validation
